fcdd594de765cd620bbe78fbf9f94b5a3582e7d6
Vittorio <tonibusatta@gmail.com>
2022-12-11 20:10:40 +0400

    progress of the project

---
+++ pages/mobilier/index.js
@@ -0,0 +1,84 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import styled from "styled-components";
+import { Contenthead, Filter } from "../../utils/common";
+// import img from "../../assets/images/mobilier2.png";
+import Header from "../../components/Header";
+import data from "../api/data.json";
+
+export const SubHead = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-direction: column;
+  row-gap: 45px;
+  overflow: hidden;
+  margin-bottom: 30px;
+  @media screen and (min-width: 1224px) {
+    align-items: flex-end;
+    flex-direction: row;
+    margin-bottom: 100px;
+  }
+`;
+
+export const BackCatalogue = styled.ul`
+  margin-top: 22px;
+  @media screen and (min-width: 1224px) {
+    margin-top: 50px;
+  }
+`;
+
+const Mobilier = () => {
+  const categoryArray = data.filter((item) => item.category == "Mobilier");
+
+  const MobilierHead = styled(Contenthead)`
+    h1::before {
+      content: "(${categoryArray.length})";
+    }
+  `;
+
+  return (
+    <section id="mobilier">
+      <Head>
+        <title>Mobilier | Menuiserie Artisanale</title>
+      </Head>
+
+      <Header />
+
+      <MobilierHead>
+        <h1>Mobilier</h1>
+
+        <SubHead>
+          <BackCatalogue className="backcatalogue">
+            <li className="active">Mobilier</li>
+            <li>Intérieur</li>
+            <li>Produits</li>
+            <li>Architecture</li>
+          </BackCatalogue>
+
+          <Filter>
+            <li className="active">Tous</li>
+            <li>Table</li>
+            <li>Commode</li>
+            <li>Buffet</li>
+          </Filter>
+        </SubHead>
+      </MobilierHead>
+
+      <main>
+        {categoryArray.map((i) => (
+          <div key={i.id}>
+            <Image src={i.src} alt="img" width={300} height={300} />
+            <h3>{i.name}</h3>
+            <p>{i.desc}</p>
+          </div>
+        ))}
+      </main>
+    </section>
+  );
+};
+
+export default Mobilier;
---
+++ utils/common.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Contenthead = styled.div`
+  h1 {
+    position: relative;
+    font-family: "Manrope";
+    font-weight: 800;
+    font-size: 48px;
+    text-transform: uppercase;
+    color: #6c5347;
+    @media screen and (min-width: 1224px) {
+      font-size: 110px;
+    }
+    &::before {
+      position: absolute;
+      top: -20px;
+      left: 0;
+      font-family: "Manrope";
+      font-weight: 800;
+      font-size: 14px;
+      line-height: 33px;
+      letter-spacing: 0.05em;
+      text-transform: uppercase;
+      color: #d9a672;
+      @media screen and (min-width: 1224px) {
+        font-size: 24px;
+        top: 0px;
+      }
+    }
+  }
+`;
+
+export const Filter = styled.ul`
+  background-color: #e1d2c1;
+  display: inline-flex;
+  column-gap: 40px;
+  padding: 6px 20px;
+  cursor: grab;
+  @media screen and (min-width: 1224px) {
+    column-gap: 80px;
+    overflow-y: none;
+  }
+  li {
+    background: none;
+    font-family: "Manrope";
+    font-weight: 800;
+    font-size: 16px;
+    line-height: 28px;
+    text-transform: uppercase;
+    color: #785436;
+  }
+`;
