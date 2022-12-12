12974330f48f4fd281c3705a4be965e594686687
Vittorio <tonibusatta@gmail.com>
2022-12-12 23:39:56 +0400

    update fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/styles.scss'
+import "../styles/styles.scss";
+import { Manrope } from "@next/font/google";
 
-function MyApp({ Component, pageProps}) {
-  return <Component {...pageProps} />
+const manrope = Manrope({
+  weight: ["400", "600", "800"],
+  style: ['normal'],
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${manrope.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
