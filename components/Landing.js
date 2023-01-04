207f813fa6371ec3ca196a02bbec743f98d51d67
Vittorio <tonibusatta@gmail.com>
2023-01-04 17:05:08 +0400

    somes changes

--- components/Landing.js
+++ components/Landing.js
@@ -1,14 +1,30 @@
 import React from "react";
+import { Asterix } from "../utils/icons";
 
 const Landing = () => {
   return (
     <section className="landing">
       <div className="landing_container">
-        <h1>Décor</h1>
-        <h1>Ta Kaz</h1>
+        <div className="landing_title">
+          <h1>Décor</h1>
+          <h1>Ta Kaz</h1>
+          <Asterix />
+        </div>
+        <div className="landing_location">
+          <div className="landing_location_content">
+            <div>
+              <h5>14 rue Auguste de villele</h5>
+              <p>Saint Benoît, La Réunion</p>
+            </div>
+            <div>
+              <h5>Coordonnées</h5>
+              <p>55” 43” 3.402 E</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
-}
+};
 
 export default Landing;
