33c60d2098bf538e4a00b6583ed8472465261583
Vittorio <tonibusatta@gmail.com>
2023-01-16 10:21:22 +0400

    somes changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    baseUrl: 'https://www.decortakaz.re/',
+    siteUrl: 'https://www.decortakaz.re',
     generateRobotsTxt: true,
     ignoredExtensions: [
         'png', 'jpg', 'jpeg', 'gif', 'svg', 'pdf'
