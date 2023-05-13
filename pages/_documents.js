297d90fa9db8af30e573e362d26a2cfe191e3b61
Vittorio <tonibusatta@gmail.com>
2023-05-13 21:24:42 +0400

    fix font

---
+++ pages/_documents.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://use.typekit.net/fqg5gfj.css"
+        ></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
