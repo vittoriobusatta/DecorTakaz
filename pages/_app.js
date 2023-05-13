3d4b34de4ae03bc485125eaa7eea7c83cb58dfe1
Vittorio <tonibusatta@gmail.com>
2023-05-13 16:05:45 +0400

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import { AnimatePresence } from "framer-motion";
 import Loader from "../components/Loader";
 import "../styles/styles.scss";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <>
       {/* <Loader /> */}
+      <Analytics />
       <AnimatePresence initial={false} mode={"wait"}>
         <Component key={router.pathname} {...pageProps} />
       </AnimatePresence>
