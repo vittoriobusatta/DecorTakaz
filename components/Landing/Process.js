8f62f49038de386e3050d2b53e63f5c91c5b72a8
Vittorio <tonibusatta@gmail.com>
2023-06-23 01:53:28 +0400

    somes changes

--- components/Landing/Process.js
+++ components/Landing/Process.js
@@ -7,28 +7,28 @@ const process = [
     title: "Conception du design",
     description:
       "Nous vous proposerons différents modèles et styles afin que vous puissiez choisir celui qui correspond le mieux à vos goûts et à votre intérieur.",
-    icon: <PenIcon color={"#4e2705"} />,
+    icon: <PenIcon color={"#A66C34"} />,
   },
   {
     id: 2,
     title: "Sélection des matières",
     description:
       "Nous vous présenterons les différentes essences et leurs caractéristiques pour que vous puissiez faire un choix éclairé.",
-    icon: <LogIcon color={"#4e2705"} />,
+    icon: <LogIcon color={"#A66C34"} />,
   },
   {
     id: 3,
     title: "Fabrication",
     description:
       "Nous mettons un point d'honneur à réaliser chaque meuble de manière artisanale, en respectant les traditions de la menuiserie.",
-    icon: <ToolsIcon color={"#4e2705"} />,
+    icon: <ToolsIcon color={"#A66C34"} />,
   },
   {
     id: 4,
     title: "Livraison",
     description:
       "Nous nous chargeons de toutes les étapes de transport et d'installation afin que vous puissiez profiter pleinement de votre nouvelle pièce dès son arrivée.",
-    icon: <GiftIcon color={"#4e2705"} />,
+    icon: <GiftIcon color={"#A66C34"} />,
   },
 ];
 
