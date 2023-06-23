b878cf502a17da7a5c5286e13b4d46d7b3121736
Vittorio <tonibusatta@gmail.com>
2023-06-24 01:48:32 +0400

    somes changes

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,68 +1,9 @@
-import gsap from "gsap";
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import Head from "next/head";
-import Header from "../../components/Header";
-// import aboutImg from "../../public/images/section/about.webp";
-import aboutImgmobile from "../../public/images/section/aboutMobile.webp";
-import Image from "next/image";
-import { motion } from "framer-motion";
+import Transition from "../../utils/Transition";
 
 function About() {
   const aboutContainerRef = useRef(null);
-  const titleRef = useRef(null);
-  const subtitleRef = useRef(null);
-  const imageRef = useRef(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          animateElements();
-          observer.unobserve(entry.target);
-        }
-      });
-    });
-
-    observer.observe(aboutContainerRef.current);
-  }, []);
-
-  function animateElements() {
-    gsap.fromTo(
-      titleRef.current,
-      { y: 110, skewY: 10 },
-      {
-        delay: 0.3,
-        y: 0,
-        duration: 0.8,
-        opacity: 1,
-        skewY: 0,
-        ease: "power4.out",
-      }
-    );
-    gsap.fromTo(
-      subtitleRef.current,
-      { y: 70, skewY: 10 },
-      {
-        delay: 0.1,
-        y: 0,
-        opacity: 1,
-        skewY: 0,
-        ease: "power4.out",
-      }
-    );
-    gsap.fromTo(
-      imageRef.current,
-      { y: 0, clipPath: "polygon(0 0, 100% 0, 100% 0, 0 0)" },
-      {
-        delay: 0.5,
-        y: 0,
-        duration: 1.7,
-        opacity: 1,
-        ease: "power4.out",
-        clipPath: "polygon(0px 0px, 100% 0px, 100% 100%, 0px 100%)",
-      }
-    );
-  }
 
   return (
     <>
@@ -79,72 +20,11 @@ function About() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
-
-      <motion.section
-        initial={{
-          y: "0%",
-          opacity: 0,
-          clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
-        }}
-        animate={{
-          y: "0%",
-          opacity: 1,
-          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-        }}
-        transition={{ duration: 0.75, ease: "easeOut" }}
-        exit={{
-          opacity: 1,
-        }}
-        ref={aboutContainerRef}
-        className="about"
-      >
-        <div className="about_container">
-          <div className="about_layout_left">
-            {/* <Image src={aboutImg} alt="about" /> */}
-          </div>
-
-          <div className="about_layout">
-            <div className="about_head">
-              <div className="hidden">
-                <h5 ref={subtitleRef} className="subtitle opacity">
-                  À Propos
-                </h5>
-              </div>
-              <div className="hidden">
-                <h1 ref={titleRef} className="about_title title opacity">
-                  De nous
-                </h1>
-              </div>
-              <div className="introduction_container hidden">
-                <p className="about_introduction introduction">
-                  Fondée en 2017, Décor Ta Kaz est une entreprise d’ameublement
-                  qui est synonyme de produits de haute qualité des plus grands
-                  designs. En tant que rêveurs, créateurs, innovateurs et
-                  concepteurs, nous sommes fiers de fournir les solutions les
-                  plus efficaces, pratiques et abordables pour la vie moderne.
-                </p>
-                <p className="about_introduction introduction">
-                  En servant tous les conforts, nous cherchons à rendre chaque
-                  jour plus lisse et gérable que vous allez dans vos activités
-                  quotidiennes. Décor ta Kaz est une intégration de marques;
-                  Notre collection comprend des articles fonctionnels améliorés
-                  ainsi qu’un décor exquis - une combinaison idéale qui est mis
-                  en valeur tout espace de vie et de vie.
-                </p>
-              </div>
-            </div>
-            <Image
-              className="opacity"
-              ref={imageRef}
-              src={aboutImgmobile}
-              alt="about"
-            />
-          </div>
-        </div>
-      </motion.section>
+      <section className="about" ref={aboutContainerRef}>
+        <h1>À Propos</h1>
+      </section>
     </>
   );
 }
 
-export default About;
+export default Transition(About);
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,84 +1,9 @@
 import Head from "next/head";
-import Image from "next/image";
-import Header from "../../components/Header";
-import contactImg from "../../public/images/section/contact.webp";
-import contactImgmobile from "../../public/images/section/contactMobile.webp";
-import { FacebookIcon, InstagramIcon } from "../../utils/icons";
-import gsap from "gsap";
-import React, { useEffect, useRef } from "react";
-import { motion } from "framer-motion";
+import React, { useRef } from "react";
+import Transition from "../../utils/Transition";
 
 function Contact() {
   const contactContainerRef = useRef(null);
-  const titleRef = useRef(null);
-  const subtitleRef = useRef(null);
-  const imageRef = useRef(null);
-
-  const spanRef = useRef([]);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          animateElements();
-          observer.unobserve(entry.target);
-        }
-      });
-    });
-
-    observer.observe(contactContainerRef.current);
-  }, []);
-
-  function animateElements() {
-    gsap.fromTo(
-      titleRef.current,
-      { y: 110, skewY: 10 },
-      {
-        delay: 0.3,
-        y: 0,
-        duration: 0.8,
-        opacity: 1,
-        skewY: 0,
-        ease: "power4.out",
-      }
-    );
-    gsap.fromTo(
-      subtitleRef.current,
-      { y: 70, skewY: 10 },
-      {
-        delay: 0.1,
-        y: 0,
-        opacity: 1,
-        skewY: 0,
-        ease: "power4.out",
-      }
-    );
-    gsap.fromTo(
-      imageRef.current,
-      { y: 0, clipPath: "polygon(0 0, 100% 0, 100% 0, 0 0)" },
-      {
-        delay: 0.5,
-        y: 0,
-        duration: 1.7,
-        opacity: 1,
-        ease: "power4.out",
-        clipPath: "polygon(0px 0px, 100% 0px, 100% 100%, 0px 100%)",
-      }
-    );
-    spanRef.current.forEach((ref, index) => {
-      gsap.fromTo(
-        ref,
-        { y: 70 },
-        {
-          delay: 0.3 * index,
-          y: 0,
-          duration: 0.8,
-          opacity: 1,
-          ease: "power4.out",
-        }
-      );
-    });
-  }
 
   return (
     <>
@@ -92,93 +17,11 @@ function Contact() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
-      <motion.section
-        initial={{
-          y: "0%",
-          opacity: 0,
-          clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
-        }}
-        animate={{
-          y: "0%",
-          opacity: 1,
-          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-        }}
-        transition={{ duration: 0.75, ease: "easeOut" }}
-        exit={{
-          opacity: 1,
-        }}
-        ref={contactContainerRef}
-        className="contact"
-      >
-        <div className="contact_container">
-          <div className="contact_layout_left">
-            <Image src={contactImg} alt="contact" />
-          </div>
-
-          <div className="contact_layout">
-            <div className="contact_head">
-              <div className="hidden">
-                <h5 ref={subtitleRef} className="subtitle opacity">
-                  Nous
-                </h5>
-              </div>
-              <div className="hidden">
-                <h1 ref={titleRef} className="title opacity">
-                  Contacter
-                </h1>
-              </div>
-              <div className="introduction_container hidden">
-                <p className="contact_introduction">decortakaz@gmail.com</p>
-                <p className="contact_introduction">+262 6 92 43 51 92</p>
-                <div className="contact_adresse hidden">
-                  <div className="hidden">
-                    <p
-                      className="opacity"
-                      ref={(el) => (spanRef.current[0] = el)}
-                    >
-                      14 Rue Auguste de Villele
-                    </p>
-                  </div>
-                  <div className="hidden">
-                    <p
-                      className="opacity"
-                      ref={(el) => (spanRef.current[1] = el)}
-                    >
-                      Saint-Benoît 97470,
-                    </p>
-                  </div>
-
-                  <div className="hidden">
-                    <p
-                      className="opacity"
-                      ref={(el) => (spanRef.current[2] = el)}
-                    >
-                      La Réunion
-                    </p>
-                  </div>
-                </div>
-                <div className="contact_socials">
-                  <a href="https://www.facebook.com/people/D%C3%A9cor-ta-Kaz/100064274190012/">
-                    <FacebookIcon />
-                  </a>
-                  <a href="https://www.instagram.com/decortakaz/">
-                    <InstagramIcon />
-                  </a>
-                </div>
-              </div>
-            </div>
-            <Image
-              className="opacity"
-              ref={imageRef}
-              src={contactImgmobile}
-              alt="contact"
-            />
-          </div>
-        </div>
-      </motion.section>
+      <section className="contact" ref={contactContainerRef}>
+        <h1>Contact</h1>
+      </section>
     </>
   );
 }
 
-export default Contact;
+export default Transition(Contact);
