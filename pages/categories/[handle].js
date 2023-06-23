9ad8433a8c80f5dfe94248ac888eeccae9cbeb07
Vittorio <tonibusatta@gmail.com>
2023-06-24 02:25:41 +0400

    try to fix

--- pages/categories/[handle].js
+++ pages/categories/[handle].js
@@ -2,7 +2,31 @@ import axios from "axios";
 import React from "react";
 import Head from "next/head";
 import Gallery from "../../components/Gallery";
-import Transition from "../../utils/Transition";
+import { motion } from "framer-motion";
+
+function Transition(Component) {
+  return function TransitionComponent(props) {
+    return (
+      <>
+        <Component {...props} />
+        <motion.div
+          className="slide-in"
+          initial={{ scaleY: 0 }}
+          animate={{ scaleY: 0 }}
+          exit={{ scaleY: 1 }}
+          transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+        ></motion.div>
+        <motion.div
+          className="slide-out"
+          initial={{ scaleY: 1 }}
+          animate={{ scaleY: 0 }}
+          exit={{ scaleY: 0 }}
+          transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+        ></motion.div>
+      </>
+    );
+  };
+}
 
 const domain = process.env.NEXT_PUBLIC_HOSTNAME;
 
