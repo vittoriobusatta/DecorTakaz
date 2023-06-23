692c5e45f6df6f93fb7ad393583cbcd6125851f1
Vittorio <tonibusatta@gmail.com>
2023-06-24 02:04:42 +0400

    try fix

--- utils/transition.js
+++ utils/transition.js
@@ -1,34 +1,26 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const Transition = (Component) => {
-  const TransitionComponent = (props) => (
-    <>
-      <Component {...props} />
-      <motion.div
-        className="slide-in"
-        initial={{ scaleY: 0 }}
-        animate={{ scaleY: 0 }}
-        exit={{ scaleY: 1 }}
-        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
-      >
-      </motion.div>
-      <motion.div
-        className="slide-out"
-        initial={{ scaleY: 1 }}
-        animate={{ scaleY: 0 }}
-        exit={{ scaleY: 0 }}
-        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
-      >
-      </motion.div>
-    </>
-  );
-
-  TransitionComponent.displayName = `Transition(${
-    Component.displayName || Component.name || "Component"
-  })`;
-
-  return TransitionComponent;
-};
-
-export default Transition;
+export default function Transition(Component) {
+  return function TransitionComponent(props) {
+    return (
+      <>
+        <Component {...props} />
+        <motion.div
+          className="slide-in"
+          initial={{ scaleY: 0 }}
+          animate={{ scaleY: 0 }}
+          exit={{ scaleY: 1 }}
+          transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+        ></motion.div>
+        <motion.div
+          className="slide-out"
+          initial={{ scaleY: 1 }}
+          animate={{ scaleY: 0 }}
+          exit={{ scaleY: 0 }}
+          transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+        ></motion.div>
+      </>
+    );
+  };
+}
