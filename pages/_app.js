89a59e985e2c85258ad8c0159f6b7a3345659ad8
Vittorio <tonibusatta@gmail.com>
2023-08-19 22:55:29 +0400

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import { AnimatePresence } from "framer-motion";
 import Loader from "../components/Loader";
 import "../styles/styles.scss";
-import { Analytics } from "@vercel/analytics/react";
 import Layout from "../components/Layout";
 import { useEffect } from "react";
 import Lenis from "@studio-freight/lenis";
@@ -37,7 +36,6 @@ function MyApp({ Component, pageProps, router }) {
   return (
     <>
       {/* <Loader /> */}
-      <Analytics />
       <AnimatePresence mode={"wait"}>
         <Layout>
           <Component key={router.pathname} {...pageProps} />
